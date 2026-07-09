ac3935b5032afe711f8dac97ee7d88d9a8672f5b
xxxxffniy <rogerious84@protonmail.com>
2026-07-09 07:42:00 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const TELEGRAM_TOKEN = "6968043726:AAHGJ0OtlbTMjTOPTfacioSJqOwgePt0Xhw";
-export const CHAT_ID = 5710607863;
+export const TELEGRAM_TOKEN = "8635814434:AAGOf_KhWRWvSExATPV0MV2RUkPcGyVFWlA";
+export const CHAT_ID = 7559269577;
 export const IP_TOKEN = "34a50f68edba06";
